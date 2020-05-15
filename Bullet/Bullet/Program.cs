d05d2d2a01f832649b1033b4a047e93903acbb42
Nafen-source <65283579+Nafen-source@users.noreply.github.com>
2020-05-15 13:21:57 +0100

    Add files via upload

--- Bullet/Bullet/Program.cs
+++ Bullet/Bullet/Program.cs
@@ -12,16 +12,17 @@ namespace Bullet
         public int posX { get => _posX; set => _posX = value; }
         public int posY { get => _posY; set => _posY = value; }
 
-        public Bullet(int posY, int posX)
+        public Bullet(int posY, int posX, int direction)
         {
             _posX = posX;
             _posY = posY;
+            _direction = direction;
         }
 
         public void Move()
         {
-            // Moves bullet up by 1
-            _posY = _posY - 1;
+            // Moves bullet by 1
+            _posY = _posY + _direction;
         }
 
         public bool OffScreen()
@@ -31,6 +32,33 @@ namespace Bullet
                 return false;
             }
             return true;
+        }
+    }
+
+    class Alien
+    {
+        private int _posY;
+        private int _posX;
+
+        public int posY { get => _posY; }
+        public int posX { get => _posX; }
+
+        public Alien(int posX, int posY)
+        {
+            _posX = posX;
+            _posY = posY;
+        }
+
+        public void Shoot()
+        {
+            // creates bullet at that aliens position and moving down
+            Bullet bullet1 = new Bullet(_posY, _posX, 1);
+        }
+
+        public void SetPosition(int posY, int posX)
+        {
+            _posY = posY;
+            _posX = posX;
         }
     }
 }
